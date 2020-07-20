88268f3f39df6c74217f2efd2128e46c2f19e45f
rafitc <rafi@Rafis-MacBook-Air.local>
2020-07-20 15:39:50 +0530

    update card background with temp

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <WeatherCard/>
+      <WeatherCard temp='60'/>
+           
+      <WeatherCard temp='00'/>
       
     </div>
   );
--- src/components/WeatherCard/component.js
+++ src/components/WeatherCard/component.js
@@ -6,10 +6,31 @@ import Condition from './condition';
 import Icon from './Icon';
 
 const WeatherCard = (props) => {
-    const red = 200;
+    let highColor = 0;
+    let lowColor = 0;
+    let bg = null;
+    if(props.temp > 12){
+        highColor = (1-((props.temp - 12)/28)) * 255;
+        lowColor = (highColor - 150);
+        bg = `linear-gradient(
+            to top, 
+            rgb(255, ${ highColor }, 0), 
+            rgb(255, ${ lowColor }, 0)
+            );`
+    }
+    else if(props.temp <= 12){
+        highColor = (1-((props.temp + 12)/28)) * 255;
+        lowColor = (highColor - 150);
+        bg = `linear-gradient(
+            to top, 
+            rgb(0, ${ highColor }, 255), 
+            rgb(0, ${ lowColor }, 255)
+            );`
+    }
+
     const Card = styled.div`
         margin: 0 auto;
-        background: linear-gradient(to bottom, rgba(${red},200,200), lightblue);
+        background: ${bg} ;
         width: 200px;
         height: 280px;
         display: flex;
